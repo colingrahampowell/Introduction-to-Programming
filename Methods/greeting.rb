0c3c23486183cc0785756b3a0e0668f5674c6f86
Colin Powell <colingrahampowell@gmail.com>
2015-09-10 23:31:16 -0400

    Tealeaf Intro to Programming - Methods

---
+++ Methods/greeting.rb
@@ -0,0 +1,8 @@
+def greeting(name)
+  puts "Hey, #{name}, what's up?"
+end
+
+puts "Hey dude, who are you?"
+get_name = gets.chomp
+greeting(get_name)
+
---
+++ Methods/scream.rb
@@ -0,0 +1,9 @@
+# Tealeaf Intro to Programming exercise.
+# Adds exclamation!!!! points!!!! to a string. 
+
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
